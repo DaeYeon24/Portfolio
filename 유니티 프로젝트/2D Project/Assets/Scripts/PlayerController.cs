6b0deb2e4e7d435fd2efd0dfec92fdaf48400f8c
DaeYeon24 <66584716+DaeYeon24@users.noreply.github.com>
2021-04-02 06:10:53 +0900

    Update 21.04.02 (2D 프로젝트 시작)

---
+++ 유니티 프로젝트/2D Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRigidbody;
+    SpriteRenderer spriteRenderer;
+    Animator playerAnimator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if (playerRigidbody.velocity.x == 0)
+            playerAnimator.SetBool("isRun", false);
+        else
+            playerAnimator.SetBool("isRun", true);
+    }
+    void FixedUpdate()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+
+        playerRigidbody.velocity = new Vector2(xInput, 0) * 150f * Time.deltaTime;
+        //playerRigidbody.AddForce(new Vector2(xInput, 0), ForceMode2D.Impulse);
+    }
+}
