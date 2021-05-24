a5dfc25f58d97a845326b3af762b24a6a128b98b
DaeYeon24 <66584716+DaeYeon24@users.noreply.github.com>
2021-05-24 21:14:27 +0900

    Update 21.05.24 (탑다운 프로젝트 마무리)

--- 유니티 프로젝트/2D Topdown RPG/Assets/Scripts/ObjectData.cs
+++ 유니티 프로젝트/2D Topdown RPG/Assets/Scripts/ObjectData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObjectData : MonoBehaviour
 {
+    // 오브젝트 아이디와 NPC 유무
     public int id;
     public bool isNPC;
 }
--- 유니티 프로젝트/2D Topdown RPG/Assets/Scripts/PlayerCtrl.cs
+++ 유니티 프로젝트/2D Topdown RPG/Assets/Scripts/PlayerCtrl.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    Rigidbody2D rigid;
-    Animator anim;
-    Vector3 frontVec;
-    GameObject scanObject;
-    float xInput;
-    float zInput;
-    int speed;
-    bool isHorizonMove;
+    Rigidbody2D rigid; // 리지드바디
+    Animator anim; // 애니메이터
+    Vector3 frontVec; // 플레이어 전방 벡터
+    GameObject scanObject; // 스캔할 오브젝트
+    float xInput; //x축 방향
+    float zInput; //z축 방향
+    int speed; // 스피드
+    bool isHorizonMove; // 수평이동 bool 변수
 
     void Awake()
     {
+        // 초기화
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         speed = 10;
@@ -22,18 +23,20 @@ public class PlayerCtrl : MonoBehaviour
 
     void Update()
     {
+        // 상호작용중이 아니면 이동 가능
         if(!GameManager.instance.isInteraction)
         {
+            // 방향키 입력
             xInput = Input.GetAxisRaw("Horizontal");
             zInput = Input.GetAxisRaw("Vertical");
-
+            // 수평이동 체크
             if (Input.GetButtonDown("Horizontal"))
                 isHorizonMove = true;
             else if (Input.GetButtonDown("Vertical"))
                 isHorizonMove = false;
             else if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
                 isHorizonMove = xInput != 0;
-
+            // 전방벡터 체크
             if (Input.GetButtonDown("Vertical") && zInput < 0)
                 frontVec = Vector3.down;
             else if (Input.GetButtonDown("Vertical") && zInput > 0)
@@ -67,10 +70,11 @@ public class PlayerCtrl : MonoBehaviour
         Vector2 moveVec = isHorizonMove ? new Vector2(xInput, 0) : new Vector2(0, zInput);
         // 결정된 방향 이동
         rigid.velocity = moveVec * speed;
-
+        // 확인용 디버그
         Debug.DrawRay(rigid.position, frontVec * 0.7f, new Color(1, 0, 0));
+        // 레이캐스트
         RaycastHit2D scanRay = Physics2D.Raycast(rigid.position, frontVec, 0.7f, LayerMask.GetMask("Object"));
-
+        // 레이캐스트히트에 잡힌 콜라이더가 널값이 아니면 스캔오브젝트에 할당
         if (scanRay.collider != null)
             scanObject = scanRay.collider.gameObject;
         else
